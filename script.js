c4580db82b3ff7898a18f83d70444559d0e0d200
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-09-22 11:00:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let level = document.getElementById("level");
+let parent = level.parentNode;
+let item= parent.children;
+index = Array.from(item).indexOf(level);
+
+ alert("The level of the element is: "+index);
